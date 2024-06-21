48a68b286411825fc87bc786a3702419827d03d6
odylogo2 <logomuse@gmail.com>
2024-06-21 15:08:35 +0300

    Refactor enemy class and event handling

--- enemy.js
+++ enemy.js
@@ -1,15 +1,18 @@
 export class Enemy {
   constructor(name, level) {
-    this.name;
-    this.level;
-    this.calculateStats();
+      this.name = name;
+      this.level = level;
+      this.calculateStats();
+      
   }
+
   calculateStats() {
-    const randomLevel = this.level * 0.1;
-    this.attack = Math.floor(Math.random() * 10 + randomLevel * 5);
-    this.defense = Math.floor(Math.random() * 10 + randomLevel * 3);
-    this.armor = Math.floor(Math.random() * 10 + randomLevel * 2);
-    this.health = Math.floor(Math.random() * 50 + randomLevel * 10);
+      const randomLevel = this.level * 0.1;
+      //για μελλοντικη χρηση
+     // this.attack = Math.floor(Math.random() * 10 + randomLevel * 5);
+     // this.defense = Math.floor(Math.random() * 10 + randomLevel * 3);
+     // this.armor = Math.floor(Math.random() * 10 + randomLevel * 2);
+     // this.health = Math.floor(Math.random() * 50 + randomLevel * 10);
   }
 }
 
@@ -18,5 +21,11 @@ const enemyTypes = ["spider", "troll", "wolf", "crow", "orq", "goblin"];
 export function randomEnemy() {
   const randomTypeList = Math.floor(Math.random() * enemyTypes.length);
   const randomLevel = Math.floor(Math.random() * 10) + 1;
-  spawnEnemy(enemyTypes[randomTypeList], randomLevel);
+  spawnEnemy(randomTypeList, randomLevel);
+}
+
+export function spawnEnemy(randomTypeList, randomLevel) {
+  const type = enemyTypes[randomTypeList];
+  const enemy = new Enemy(type, randomLevel);
+  console.log(`A ${enemy.name} of level ${enemy.level} has appeared.`);
 }
