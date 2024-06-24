0b591918d67146e05e2c984790051d37dd22a932
odylogo2 <logomuse@gmail.com>
2024-06-24 19:48:31 +0300

    right now only one button works and only to see if the rest of the code works. it does!

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 import { eventCreationInstance } from './events.js';
 
-// Επιλέγουμε το στοιχείο κουμπιού με το id "optionA"
-const optionAButton = document.getElementById("optionA");
 
-// Προσθέτουμε έναν ακροατή για το γεγονός "click" στο κουμπί "optionA"
-optionAButton.addEventListener("click", function () {
+
+
+document.getElementById("optionA").addEventListener("click", function () {
   eventCreationInstance.handlePlayerClicks();
-  
 });
