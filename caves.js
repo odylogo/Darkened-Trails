76a790772b7752561078c2b89c80101bbb171cd9
odylogo2 <logomuse@gmail.com>
2024-06-24 19:50:40 +0300

    early stage of caves. just finished who caves will be created. who to ineract is next

--- caves.js
+++ caves.js
@@ -1,7 +1,31 @@
-export class findingCave {
-    activate() {
-      this.cave = new Cave();
-      
-      console.log("Caves discovered!");
-    }
+ class Cave {
+  constructor() {
+    this.size = this.randomCaveSize();
+    this.difficulty = this.randomCaveDifficulty();
+    this.cautionLevel= this.cautionLevel();
   }
+
+  randomCaveSize() {
+    return Math.floor(Math.random() * 5) + 1 ;
+  }
+
+  randomCaveDifficulty() {
+    return Math.floor(Math.random() * 5) + 1 ;
+  }
+
+ 
+cautionLevel() {
+  if (this.difficulty === 5) {
+    return "extreme caution!! Maybe don't go in?";
+  }else if (this.difficulty >= 3) {
+    return "very carefully.";
+  }else {return "with caution.";}
+}
+}
+
+
+export function findingCave() {
+  const cave = new Cave();
+ return console.log(`You found a cave size of  ${cave.size} and difficulty ${cave.difficulty} proceed ${cave.cautionLevel}`);
+}
+
